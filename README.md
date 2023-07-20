ffb091095d061e7f70a2d678cb3a242b778b40a5
Chebure4ik <darikmlk@gmail.com>
2023-07-20 21:00:27 +0600

    commit200723

---
+++ README.md
@@ -0,0 +1,8 @@
+# SeventhLessonGamePilot
+<p align="left">
+<img src="https://user-images.githubusercontent.com/108148690/227385754-8f92d42b-4973-41ed-bfc1-d3af150c66a1.jpeg"/>
+</p>
+<p align="left">
+<img src="https://user-images.githubusercontent.com/108148690/227385319-7a11d730-ff76-4307-96ee-5cfbc3bda6cd.jpeg"/>
+</p>
+<p><span style="color: #046e7e;"><em>Часть кода взята и процитирована в учебных целях у автора <strong>heyletscode</strong>&nbsp;по общедоступной ссылке</em>&nbsp;<strong>https://github.com/heyletscode/2D-Game-In-Android-Studio.git</strong></span></p>
---
+++ app/src/main/java/com/msaggik/seventhlessongamepilot/MainActivity.java
@@ -0,0 +1,29 @@
+package com.msaggik.seventhlessongamepilot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // определение полноэкранного режима
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // обработка нажатия на TextView с id play
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // переключение активити с помощью намерения
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+    }
+}
